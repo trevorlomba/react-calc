7c5dd88e0987e0ec607ed464673996053f4ef65c
trevorlomba <trevorlomba@gmail.com>
2022-02-22 01:15:06 -0500

    improving logic and readability

---
+++ react-calc/src/components/Logger.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import './Logger.css'
+
+export const Logger = props => <div className="logger"><div>COLength:{props.COLength}\\  </div><div>answer:{props.onAnswer}\\  </div><div>parenths:  {props.openParenths}\\  </div> <div>{props.inputType}\\  </div></div>
