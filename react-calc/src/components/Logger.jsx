ca23e6ebb7b4555746d5ce4d03136c9b08b7a6c5
trevorlomba <trevorlomba@gmail.com>
2022-02-23 15:50:53 -0500

    removed old dir

--- react-calc/src/components/Logger.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import './Logger.css'
-
-export const Logger = props => <div className="logger"><div>COLength:{props.COLength}\\  </div><div>answer:{props.onAnswer}\\  </div><div>parenths:{props.openParenths}\\  </div> <div>inputType:{props.inputType}\\  </div></div>
