0d06a768bc43296a2050f1e50fc0b5902e9876d3
trevorlomba <trevorlomba@gmail.com>
2022-02-23 15:50:53 -0500

    removed old dir

--- react-calc/src/components/ClearButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './ClearButton.css';
-
-export const ClearButton = props => (
-    <div className = "clear-btn" onClick={() => props.handleClick(props.children)}> 
-    {props.children} 
-    </div>
-);
