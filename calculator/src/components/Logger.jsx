7af6770d05bc3a3407a233af5d5eec46e909c71e
trevorlomba <trevorlomba@gmail.com>
2022-02-23 22:19:58 -0500

    value look up logic and logging

--- calculator/src/components/Logger.jsx
+++ calculator/src/components/Logger.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import './Logger.css'
 
-export const Logger = props => <div className="logger"><div>COLength:{props.COLength}\\  </div><div>answer:{props.onAnswer}\\  </div><div>parenths:{props.openParenths}\\  </div> <div>inputType:{props.inputType}\\  </div></div>
+export const Logger = props => <div className="logger"><div>COLength:{props.COLength}\\  </div><div>parenths:{props.openParenths}\\  </div> <div>preValType:{props.preValType}\\  </div></div>
