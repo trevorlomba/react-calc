b847e38bc8b68fd9098c29c0f8c5001996ae4004
trevorlomba <trevorlomba@gmail.com>
2022-02-24 00:14:05 -0500

    collapsed handleEqual and handleClear functions into updateInput

--- calculator/src/valTypes.js
+++ calculator/src/valTypes.js
@@ -1,86 +1,93 @@
 export const decimal = {
-    'COFloat': 'break',
-    'answer': 'decimal',
-    'CO': 'COFloat',
-    'decimal': 'break',
-    'empty': 'decimal',
-    'operator': 'decimal,',
-    'minus': 'decimal',
-    'openParenths': 'decimal'
+    test: ['test', false, 1],
+    COFloat: ['break'],
+    answer: ['decimal', true],
+    CO: ['COFloat'],
+    decimal: ['break'],
+    empty: ['decimal'],
+    operator: 'decimal,',
+    minus: ['decimal'],
+    openParenths: ['decimal'],
 }
 
 export const closedParenths = {
-    'COFloat': 'CO',
-    'answer': 'CO',
-    'CO': 'CO',
-    'decimal': 'CO',
-    'empty': 'CO',
-    'operator': 'CO',
-    'minus': 'break',
-    'openParenths': 'break'
+    parenths: -1,
+    COFloat: ['CO'],
+    answer: ['CO'],
+    CO: ['CO'],
+    decimal: ['CO'],
+    empty: ['CO'],
+    operator: ['CO'],
+    minus: ['break'],
+    openParenths: ['break'],
 }
 
 export const minus = {
-    'COFloat': 'minus',
-    'answer': 'operator',
-    'CO': 'minus',
-    'empty': 'minus',
-    'decimal': 'break',
-    'operator': 'operator',
-    'minus': 'break',
-    'openParenths': 'minus'
+    COFloat: ['minus'],
+    answer: ['operator'],
+    CO: ['minus'],
+    empty: ['minus'],
+    decimal: ['break'],
+    operator: ['operator', false, 'preVal'],
+    minus: ['break'],
+    openParenths: ['minus'],
 }
 
 export const operator = {
-    'COFloat': 'operator',
-    'answer': 'operator',
-    'CO': 'operator',
-    'empty': 'break',
-    'decimal': 'break',
-    'operator': 'operator',
-    'minus': 'operator',
-    'openParenths': 'break'
+    COFloat: ['operator'],
+    answer: ['operator'],
+    CO: ['operator'],
+    empty: ['break'],
+    decimal: ['break'],
+    operator: ['operator', false, 'preVal'],
+    minus: ['operator', false, 'preVal'],
+    openParenths: ['break'],
 }
 
 export const CO = {
-    'COFloat': 'COFloat',
-    'answer': 'CO',
-    'CO': 'CO',
-    'empty': 'CO',
-    'decimal': 'COFloat',
-    'operator': 'CO',
-    'minus': 'CO',
-    'openParenths': 'CO'
+    COFloat: ['COFloat'],
+    answer: ['CO', true],
+    CO: ['CO'],
+    empty: ['CO'],
+    decimal: ['COFloat'],
+    operator: ['CO'],
+    minus: ['CO'],
+    openParenths: ['CO'],
 }
 
 export const openedParenths = {
-    'COFloat': 'openParenths',
-    'answer': 'openParenths',
-    'CO': 'openParenths',
-    'empty': 'openParenths',
-    'decimal': 'break',
-    'operator': 'openParenths',
-    'minus': 'openParenths',
-    'openParenths': 'openParenths'
+    parenths: 1,
+    COFloat: ['openParenths'],
+    answer: ['openParenths', true],
+    CO: ['openParenths'],
+    empty: ['openParenths'],
+    decimal: ['break'],
+    operator: ['openParenths'],
+    minus: ['openParenths'],
+    openParenths: ['openParenths'],
 }
 
 export const equals = {
-    'COFloat': 'answer',
-    'answer': 'answer',
-    'CO': 'answer',
-    'empty': 'answer',
-    'decimal': 'break',
-    'operator': 'answer',
-    'minus': 'break',
-    'openParenths': 'break'
+    doMath: 1,
+    COLength: 0,
+    COFloat: ['answer', true],
+    answer: ['break', true],
+    CO: ['answer', true],
+    empty: ['answer', true],
+    decimal: ['break', true],
+    operator: ['answer', false, 'preVal'],
+    minus: ['break', true],
+    openParenths: ['break', true],
 }
 
-module.exports = {
-    equals,
-    decimal,
-    closedParenths,
-    minus,
-    operator,
-    CO,
-    openedParenths
+
+export const clear = {
+    COFloat: ['answer', false, 'COL'],
+    answer: ['answer', true],
+    CO: ['answer', false, 'COL'],
+    empty: ['answer', false, 'COL'],
+    decimal: ['break', false, 'COL'],
+    operator: ['answer', false, 'COL'],
+    minus: ['break', false],
+    openParenths: ['answer', true],
 }
